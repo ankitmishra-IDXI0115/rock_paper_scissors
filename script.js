53797276abe1eeec0195034f4f26615effb1172d
Ankit Mishra <ankit.mishra@indexnine.com>
2026-06-29 22:25:18 +0530

    getComputerChoice Done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice() {
+
+    let num = Math.floor(Math.random() * 3);
+
+    if (num === 0) {
+        return "rock";
+    } 
+    else if (num === 1) {
+        return "paper";
+    } 
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
